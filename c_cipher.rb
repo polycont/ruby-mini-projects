7622d666bfd0979840ec2b17cee3bb953cb76f21
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-09-05 00:06:43 -0500

    Finish caesar cipher proj

---
+++ c_cipher.rb
@@ -0,0 +1,23 @@
+def caesar_cipher(string, shift)
+  alphabet = ('a'..'z').to_a
+  new_string = []
+  string = string.split('').each { |letter| 
+    if alphabet.include?(letter) == false && alphabet.include?(letter.downcase) == false
+      new_string << letter
+    elsif letter == letter.upcase
+      letter = letter.downcase
+      index = alphabet.index(letter)
+      index = (index + shift) % 26
+      index = alphabet[index]
+      new_string << index.upcase
+    else
+      letter = letter.downcase
+      index = alphabet.index(letter)
+      index = (index + shift) % 26
+      index = alphabet[index]
+      new_string << index
+    end
+  }
+  new_string = new_string.join('')
+end
+p caesar_cipher("Testing, TeStInG, 1, 5, >, ... ???", 2)
